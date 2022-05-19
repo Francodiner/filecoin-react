2efdd056afac7af43ad880cb3a68b581f8a642ee
Franco Diner <franco.diner@dracoder.com>
2022-05-19 10:58:38 -0300

    v2.0.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { createPow, powTypes } from "@textile/powergate-client"
 import { useEffect, useState } from 'react';
+import { Alert, Button, Table, Form } from 'react-bootstrap'
 
 function App() {
 
@@ -11,13 +12,20 @@ function App() {
   const [address, setAddress] = useState('');
   const [cid, setCid] = useState('');
   const [jobId, setjobId] = useState('');
+  const [fileName, setFileName] = useState('');
   const [selectedFile, setSelectedFile] = useState();
+  const [fileStatus, setFileStatus] = useState('');
   const [isFilePicked, setIsFilePicked] = useState(false);
 
   const pow = createPow({ host })
 
+  useEffect(()=> {
+    pow.setAdminToken("f75cc186-8ccb-4d12-a0db-3ba2ceb5974b")
+  }) 
+
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
+    setFileName(event.target.files[0].name)
     setIsFilePicked(true);
   };
 
@@ -28,7 +36,6 @@ function App() {
           setUserToken(res.user.token)
           setUserId(res.user.id)
 
-          pow.setAdminToken("f75cc186-8ccb-4d12-a0db-3ba2ceb5974b")
           pow.setToken(res.user.token)
 
           pow.wallet.addresses().then(
@@ -49,12 +56,10 @@ function App() {
   }
 
   async function subirArchivo() {
-    pow.setAdminToken("f75cc186-8ccb-4d12-a0db-3ba2ceb5974b")
     pow.setToken(userToken)
 
     pow.data.stage(selectedFile).then(
       res => {
-        console.log(res.cid)
         setCid(res.cid);
         setjobId(pow.storageConfig.apply(res.cid))
       },
@@ -64,13 +69,12 @@ function App() {
     )
   }
 
-  async function getDataArchivo(){
-    pow.setAdminToken("f75cc186-8ccb-4d12-a0db-3ba2ceb5974b")
+  async function getDataArchivo() {
     pow.setToken(userToken)
 
     pow.data.cidInfo(cid).then(
       res => {
-        console.log(res)
+        setFileStatus(res.cidInfo.executingStorageJob.status)
       },
       err => {
         console.error(err)
@@ -80,28 +84,69 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Crear usuario</h2>
-        <button onClick={crearUsuario}>
-          Crear Usuario
-        </button>
-        <h3>Token de Usuario: {userToken}</h3>
-        <h3>Id de Usuario: {userId}</h3>
-        <h3>Wallet Addres: {address}</h3>
+    <div className='background-img'>
+      <div className="App">
+        <Alert variant="info">
+          <Alert.Heading>Open4Blockchain - Filecoin Powergate</Alert.Heading>
+          <p>
+            Powergate nos brinda una interfaz más sencilla y un mejor rendimiento de Filecoin.
+            Podemos administrar nuestros nodos y también obtener acceso a configuraciones de almacenamiento como por ejemplo selección de minero.
+          </p>
+          <hr />
+          <p className="mb-0">Seguir los pasos a continuación...</p>
+        </Alert>
+        <div className='info'>
+          <Button variant="light" className='mt-top' onClick={crearUsuario}>Crear Usuario</Button>
+          <div className='container'>
 
-        <h2>Subir archivo</h2>
-        <div>
-          <input type="file" name="file" onChange={changeHandler} />
-          <div>
-            <button onClick={subirArchivo}>Submit</button>
+            <Table striped bordered hover variant="dark" className='mt-top'>
+              <thead>
+                <tr>
+                  <th>Token de Usuario</th>
+                  <th>Id de Usuario</th>
+                  <th>Wallet Addres</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{userToken}</td>
+                  <td>{userId}</td>
+                  <td>{address}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <hr />
           </div>
-          <div>
-            <button onClick={getDataArchivo}>Ver Archivo</button>
+          <div className='container'>
+            <h2 className='mt-top'>Subir archivo</h2>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Control type="file" onChange={changeHandler} />
+            </Form.Group>
+            <Button variant="light" onClick={subirArchivo}>Enviar</Button>
+            <hr />
+            <h2 className='mt-top'>Listado de archivos</h2>
+            <Table striped bordered hover variant="dark" className='mt-top'>
+              <thead>
+                <tr>
+                  <th>Filename</th>
+                  <th>CID de archivo</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{fileName}</td>
+                  <td>{cid}</td>
+                  <td>{fileStatus}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <Button variant="light" onClick={getDataArchivo}>Comprobar subida</Button>
           </div>
         </div>
-      </header>
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/Powergate/index.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import './index.css';
+import { createPow, powTypes } from "@textile/powergate-client"
+import Button from 'react-bootstrap/Button';
+
+const host = "http://localhost:6002";
+
+const pow = createPow({ host });
+
+const Powergate = () => {
+  const [file, setFile] = useState(null);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+
+      const { status } = await pow.health.check();
+      console.log('status..', status);
+      const reader = new FileReader();
+
+      reader.readAsArrayBuffer(file);
+
+      reader.onload = async () => {
+        const arrayBuffer = reader.result;
+        const buffer = new Buffer(arrayBuffer);
+        console.log(buffer);
+        // setup - no need to generate everytime. Only for example
+        const { token } = await pow.ffs.create();
+        pow.setToken(token);
+
+        // wallet management
+        // get wallet addresses associated with your FFS instance
+        const { addrsList } = await pow.ffs.addrs();
+        console.log(addrsList);
+        const balance = await pow.wallet.balance(addrsList[0].addr);
+        console.log('balance', balance)
+
+        // create new address named myNewAddress
+        // this newly created address can be specified to be the one to use for file upload
+        const { addr } = await pow.ffs.newAddr("myNewAddress");
+
+        // Copied from the CLI... The default config of the js api doesn't work for some reason
+        // but the default storage config of CLI works with testnet
+        await pow.ffs.setDefaultStorageConfig(
+          {
+            "hot": {
+              "enabled": true,
+              "allowUnfreeze": false,
+              "ipfs": {
+                "addTimeout": 30
+              }
+            },
+            "cold": {
+              "enabled": true,
+              "filecoin": {
+                "repFactor": 1,
+                "dealMinDuration": 518400,
+                "excludedMiners": null,
+                "trustedMiners": null,
+                "countryCodes": null,
+                "renew": {
+                  "enabled": false,
+                  "threshold": 0
+                },
+                "addr": addr,
+                "maxPrice": 0
+              }
+            },
+          }
+        );
+
+        const config = await pow.ffs.defaultStorageConfig();
+        console.log(config);
+        // cache data in IPFS in preparation to store it using FFS
+        const { cid } = await pow.ffs.stage(buffer);
+
+        // initiates cold storage and deal making
+        const { jobId } = await pow.ffs.pushStorageConfig(cid);
+
+        const jobsCancel = pow.storageJobs.watch((job) => {
+          if (job.status === powTypes.JobStatus.JOB_STATUS_CANCELED) {
+            console.log("job canceled")
+          } else if (job.status === powTypes.JobStatus.JOB_STATUS_FAILED) {
+            console.log("job failed")
+          } else if (job.status === powTypes.JobStatus.JOB_STATUS_SUCCESS) {
+            console.log("job success!")
+          }
+        }, jobId)
+
+        const bytes = await pow.ffs.get(cid)
+        console.log('retrieved file', bytes);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
+  return (
+    <>
+      <h1>Upload a file to Filecoin through Powergate</h1>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="file"
+          id="powergate-file"
+          onChange={e => setFile(e.target.files[0])}
+        />
+        <Button
+          type="submit"
+        >
+          Upload File
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default Powergate;
